4dfbbf5dfd69ab53a455d5617e30ee5d8e01839a
Martin Huang <mhuangcode@gmail.com>
2020-02-01 10:53:58 -0500

    Event delegate system, Sticky behavior

---
+++ AllAssemblyRequired/Assets/Objects/StickyStuff/scripts/StickyBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isConnected = false;
+
+    private void Awake() {
+        gameObject.tag = "sticky";
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidingObject = collision.gameObject;
+        StickyBehavior state = collidingObject.GetComponent<StickyBehavior>();
+
+        if (collidingObject.tag == "sticky" && state != null && state.isConnected == false) {
+            FixedJoint joint = collidingObject.AddComponent<FixedJoint>();
+            if (joint != null && state != null) {
+                joint.connectedBody = gameObject.GetComponent<Rigidbody>();
+                state.isConnected = true;
+            }
+        }
+    }
+}
---
+++ AllAssemblyRequired/Assets/Objects/Systems/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager current; 
+    public event Action<int> onAnimationStart;
+    public event Action<int> onAnimationEnd;
+    private void Awake() {
+        current = this;
+    }
+
+    
+    public void triggerAnimationStart(int id) {
+        if (onAnimationStart != null) {
+            onAnimationStart(id);
+        }
+    }
+
+    public void triggerAnimationEnd(int id) {
+        if (onAnimationEnd != null) {
+            onAnimationEnd(id);
+        }
+    }
+}
