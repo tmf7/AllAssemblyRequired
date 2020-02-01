81926041102f79c538bbd5fca0c9cebabb9ab2df
Tom Freehill <tom@foundry45.com>
2020-02-01 15:34:44 -0500

    - main menu functionality - free particles and models assets

---
+++ AllAssemblyRequired/Assets/Scripts/MainMenu/SelfDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    [SerializeField] private float _delayBeforeDestroy = 3.0f;
+
+    private void Start()
+    {
+        Destroy(gameObject, _delayBeforeDestroy);
+    }
+}
---
+++ AllAssemblyRequired/Assets/Scripts/MainMenu/StartBlock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBlock : ClickableBlock
+{
+    [SerializeField] private SceneIndex _sceneToLoad;
+
+    public override void OnClick()
+    {
+        SceneManager.LoadScene((int)_sceneToLoad, LoadSceneMode.Single);
+    }
+}
