04eb7d7397f616e85186f8e2b04be21e19791691
Tom Freehill <tom@foundry45.com>
2020-02-01 20:43:08 -0500

    - startup menus complete - sound of music complete

--- AllAssemblyRequired/Assets/Scripts/MainMenu/MenuSelectBlock.cs
+++ AllAssemblyRequired/Assets/Scripts/MainMenu/MenuSelectBlock.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuSelectBlock : ClickableBlock
 {
     [SerializeField] private Transform _menuSpawnPoint;
     [SerializeField] private GameObject _menuPrefabToSpawn;
     [SerializeField] private ParticleSystem _destroyParticles;
+    [SerializeField] private AudioClip[] _explosionSounds;
 
     public override void OnClick()
     {
@@ -17,13 +16,17 @@ public class MenuSelectBlock : ClickableBlock
     private void DestroyOldMenu()
     {
         var allBoxes = FindObjectsOfType<ClickableBlock>();
+        var oldMenuParent = allBoxes[0].transform.root;
 
         foreach (var box in allBoxes)
         {
             var newExplosion = Instantiate(_destroyParticles, box.transform.position, box.transform.rotation);
             newExplosion.transform.localScale = box.transform.lossyScale;
+            SoundManager.Instance.PlaySoundFX(_explosionSounds[Random.Range(0, _explosionSounds.Length)], Camera.main.gameObject);
             Destroy(box.gameObject);
         }
+
+        Destroy(oldMenuParent.gameObject);
     }
 
 }
--- AllAssemblyRequired/Assets/Scripts/MainMenu/StartBlock.cs
+++ AllAssemblyRequired/Assets/Scripts/MainMenu/StartBlock.cs
@@ -5,8 +5,14 @@ public class StartBlock : ClickableBlock
 {
     [SerializeField] private SceneIndex _sceneToLoad;
 
+    private void Start()
+    {
+        SoundManager.Instance.PlayMenuMusic();
+    }
+
     public override void OnClick()
     {
+        SoundManager.Instance.PlayMainMusic();
         SceneManager.LoadScene((int)_sceneToLoad, LoadSceneMode.Single);
     }
 }
