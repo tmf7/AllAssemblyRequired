4488dd925d8fbd98202f8b0fbfe427f512917100
Tom Freehill <tom@foundry45.com>
2020-02-02 11:10:36 -0500

    Pause menu prefab and functionality [WIP]

---
+++ AllAssemblyRequired/Assets/Textures/EmissionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class EmissionController : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve _emissionStrength;
+    [SerializeField] private Color _emissiveColor = Color.green;
+
+    public float Brightness
+    {
+        set
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            Material mat = renderer.material;
+
+            float emissionStrength = Mathf.Clamp01(value);
+
+            Color finalColor = _emissiveColor * Mathf.LinearToGammaSpace(emissionStrength) * _emissionStrength.Evaluate(emissionStrength);
+
+            mat.SetColor("_EmissionColor", finalColor);
+        }
+    }
+}
